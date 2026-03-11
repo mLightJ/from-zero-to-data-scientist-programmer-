febb0fcfc3fa742710577199b479d55160c3e44b
Fyp_project <ojoel877@gmail.com>
2026-03-11 23:38:51 +0000

    understanding list operations

---
+++ 03_data_structures/01_list_operations.py
@@ -0,0 +1,18 @@
+# BUILDING A LIST MANAGER
+my_list = [10, 25, 3, 47, 8]
+my_list.append(15) # add 15 to the list
+my_list.insert(2, 99) # insert 99 at index 2
+my_list.remove(47) # remove the first occurrence of 47 from the list
+my_list.pop(3) # remove the element at index 3 and return it
+my_list.sort() # sort the list in ascending order
+my_list.reverse() # reverse the order of the list
+
+print("Final List:", my_list)
+
+
+'''
+Expected Skills from this Exercise:
+- Understanding of basic list operations such as append, insert, remove, pop, sort, and reverse.
+- Ability to manipulate lists to manage and organize data effectively.
+- Familiarity with list indexing and how to access and modify elements in a list.
+- Understanding of how to use built-in list methods to perform common tasks.'''
